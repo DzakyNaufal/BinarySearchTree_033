c0735ab846f1ee756cf009b1fb8bffb3f6873dde
DzakyNaufal <dzakynmulna@gmail.com>
2022-12-28 17:45:39 +0700

    membuat case 5

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -156,8 +156,13 @@ namespace BinarySearchTree
                             x.postorder(x.ROOT);
                         }
                         break;
-
-
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid Option");
+                            break;
+                        }
                 }
             }
         }
