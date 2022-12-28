04d02cae7eac3cc17de97a340ed911e8f9d17d7b
DzakyNaufal <dzakynmulna@gmail.com>
2022-12-28 17:13:49 +0700

    membuat public program

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -21,9 +21,16 @@ namespace BinarySearchTree
         }
     }
 
-    /* A node class consists of three things, the information, references to the right child, and references to the left child */
-    internal class Program
+    /* A node class consists of three things, the information,
+     * references to the right child, and references to the left child */
+    class Program
     {
+        public Node ROOT;
+        public Program()
+        {
+            ROOT = null; /* Initializing ROOT to null */
+        }
+
         static void Main(string[] args)
         {
         }
