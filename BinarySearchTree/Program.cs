7ec9c4d8d4aed33b6ecdde31db35ff3af6f51fe5
DzakyNaufal <dzakynmulna@gmail.com>
2022-12-28 17:12:05 +0700

    membuat public Node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,8 +8,20 @@ namespace BinarySearchTree
 {
     class Node
     {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
 
+        // Constructor for the Node Class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
     }
+
+    /* A node class consists of three things, the information, references to the right child, and references to the left child */
     internal class Program
     {
         static void Main(string[] args)
